afbadbce68cca2ded6e204612737b75ab74bbe5a
Rahim Dharssi <rahim.dharssi@gmail.com>
2016-01-07 11:06:11 -0800

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,4 @@
-# Default is World
-# Author: Rahim Dharssi (rahim@somewhere.com)
-name = ARGV.first || "World"
+puts "What's your name"
+my_name = gets.strip
 
-puts "Hello, #{name}!"
+puts "Hello, #{my_name}!"
